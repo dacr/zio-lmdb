e76aea4fa0b77855dc40dfd41602e98a98c7197d
crosson david lanfeust <crosson.david@gmail.com>
2025-04-05 12:20:06 +0200

    minor cleanup

--- src/main/scala/zio/lmdb/LMDBCodec.scala
+++ src/main/scala/zio/lmdb/LMDBCodec.scala
@@ -1,9 +1,6 @@
 package zio.lmdb
 
-import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder}
-
 import java.nio.ByteBuffer
-import java.nio.charset.StandardCharsets
 
 trait LMDBCodec[T] {
   def encode(t: T): Array[Byte]
